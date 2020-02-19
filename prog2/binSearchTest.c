30b61ccc0f1767e5a471c3e78006717eb8a05605
Sarah Larkin <selarkin@mtu.edu>
2020-02-19 16:46:13 -0500

    Binary search is working now

---
+++ prog2/binSearchTest.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long linSearch(long i, long *x, long ix, long *y, long iy)
+{
+    long k = 0;
+    while (k < iy && x[i] > y[k])
+    {
+        k++;
+    }
+    return k + i;
+}
+
+long diff(long a, long b)
+{
+    if ((a - b) < 0)
+        return b - a;
+    else
+        return a - b;   
+}
+
+long binSearch(long i, long *x, long ix, long *y, long iy)
+{
+    long k = 0;
+    long j = iy - 1;
+    long m = (j - k)/2 + 1;
+    int n = 0;
+    if (x[i] < y[0])
+        return i;
+    if (x[i] > y[iy - 1])
+        return i + iy;
+    printf("%ld  %ld  i = %ld\n", x[i], y[iy-1], i);
+    printf("k = %ld  j = %ld  m = %ld\n", k, j, m);
+    while (k < j)
+    {
+        if (x[i] < y[m])
+        {
+            /* Guessed too high */
+            j = m - 1;
+            m = (j - k)/2 + 1;
+        } else {
+            /* Guessed too low */
+            k = m + 1;
+            m = m + (j - k)/2 + 1;
+        }
+/*        m = (j - k)/2;*/
+        printf("k: %d  j: %d  m: %d\n", k, j, m);
+        if (diff(k, j) < 1)
+            break;
+        if (n++ > 12)
+            break;
+    }
+    printf("k = %ld  i = %ld\n", k, i);
+    return k + i;
+}
+
+
+int main(void)
+{
+    long x1[5] = {1, 2, 3, 6, 9};
+    long y1[6] = {-1, 4, 5, 7, 8, 10 };
+    int i = 0;
+    long r = 0;
+    for (i = 0; i < 6; i++)
+    {
+        r = linSearch(i, y1, 6, x1, 5);
+        printf("x[%d] goes in spot %d\n", i, r);
+    }
+    for (i = 0; i < 6; i++)
+    {
+        r = binSearch(i, y1, 6, x1, 5);
+        printf("x[%d] goes in spot %d\n", i, r);
+    }
+
+
+    return 0;
+}
---
+++ prog2/merge.c
@@ -0,0 +1,92 @@
+/* ------------------------------------------------------------------------- */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+/**
+ * Merge.c gets x and y as args as well as a key to shared memory
+ *
+ * Communication protocol:
+ * 1 - size of x
+ * 2 - size of y
+ * note: size of returned array will be size(x) + size(y) + 1
+ * 3 - key x
+ * 4 - key y
+ * 5 - key output
+ * 
+ */
+
+int main (int argc, char **argv)
+{
+    char buf[80];
+    char *msg = "Incorrect num args:  found";
+
+    int shmIDx;
+    int shmIDy;
+    int shmIDres;
+    key_t key;
+    long *x;
+    long *y;
+    long *res;   
+
+    /* Confirm the args are all here! */
+    if (argc < 6 || argc > 6)
+    {
+        sprintf(buf, "%6s$$$ M-PROC(%4d) %s %d expected 6\n", "", getpid(), 
+                msg, argc);
+        write(1, buf, strlen(buf));
+        exit(0);
+    }
+
+    /* get and attach to shared memory with checks */   
+    key = ftok("./", argv[3]);
+    if (key == -1)
+        exit(2);
+    shmIDx = shmget(key, sizeof(long) * atol(argv[1]), 0666);
+    
+    /* check for error - exiting 2 for shared mem not grabbed */
+    if (shmIDx == -1)
+        exit(2);
+    x = (long *) shmat(shmIDx, NULL, 0);
+    if ((int) x < 0)
+    {
+        /* TODO: clean up and exit */
+        shmdt(x);
+        exit(2);
+    }
+
+    /* fork m + n kiddos to do the work */
+    
+    shmdt(x);
+    return 0;
+}
+
+
+int merge(long i, long *x, long *y, long *r, long ix, long iy, long ir) 
+{
+    long j, k;
+    if (x[i] < y[0])
+        r[i] = x[i];
+    else if (x[i] > y[iy - 1])
+        r[i] = x[i+iy];
+    else
+        r[binSearch(i, x, ix, y, iy)] = x[i];
+    return;
+}
+
+long binSearch(long i, long *x, long ix, long *y, long iy)
+{
+    long k = 0;
+    for (k = 0; k < iy; k++)
+    {
+        
+    }
+}
+
+
+
